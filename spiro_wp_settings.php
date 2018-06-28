06d48918d1332a359017d82ec6f8db7d79dc596d
Thorbjörn Sundragon <puggan@spiro.se>
2018-06-28 15:24:51 +0200

    Sort Options

--- spiro_wp_settings.php
+++ spiro_wp_settings.php
@@ -8,36 +8,40 @@ Author:      Jens Hellqvist
 Author URI:  http://superautomatic.com
 */
 
+/**
+ * Executed on register_activation
+ * @hook register_activation_hook
+ */
 function set_superautomatic_defaults()
 {
 	global $wpdb;
 
-	$o = array(
+	$o = [
 		'avatar_default' => 'mystery',
 		'avatar_rating' => 'G',
 		'category_base' => '',
 		'comment_max_links' => 2,
+		'comment_moderation' => 0, // An administrator must always approve the comment
+		'comment_whitelist' => 0, // Comment author must have a previously approved comment
+		'comments_notify' => 0,
 		'comments_per_page' => 0,
 		'date_format' => 'j F Y',
-		'time_format' => 'H:i',
-		'default_ping_status' => 'open',
-		'default_post_edit_rows' => 30,
 		'default_comment_status_page' => 'closed',
-		'embed_size_w' => 460,
+		'default_ping_status' => 'open',
+		'default_pingback_flag' => 0, // Attempt to notify any blogs linked to from the article.
+		'default_post_edit_rows' => 30,
 		'embed_size_h' => 1000,
+		'embed_size_w' => 460,
 		'links_updated_date_format' => 'j F Y, H:i',
 		'permalink_structure' => '/%category%/%postname%/',
 		'rss_language' => 'sv',
 		'start_of_week' => 1,
-		'timezone_string' => 'Europe/Stockholm',
 		'thread_comments' => 0,
-		'comments_notify' => 0,
-		'comment_moderation' => 0, // An administrator must always approve the comment
-		'comment_whitelist' => 0, // Comment author must have a previously approved comment
-		'default_pingback_flag' => 0, // Attempt to notify any blogs linked to from the article.
+		'time_format' => 'H:i',
+		'timezone_string' => 'Europe/Stockholm',
+		'uploads_use_yearmonth_folders' => 0, // Organize my uploads into month- and year-based folders
 		'use_smilies' => 0,
-		'uploads_use_yearmonth_folders' => 0 // Organize my uploads into month- and year-based folders
-	);
+	];
 
 	foreach ( $o as $k => $v )
 	{
